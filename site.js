12aa9bc255d2bab4fde10358aa4bda44f6cd9219
Mitch Morganti <rmorgantistvm@gmail.com>
2019-11-02 12:38:44 -0400

    added bounds lmao

--- site.js
+++ site.js
@@ -1,11 +1,17 @@
 // JavaScript source code
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
+    // The location of Ohio State
     var OhioState = { lat: 39.999, lng: -83.013 };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), { zoom: 16, center: OhioState });
+
+    // The map, centered at Ohio State
+    //Initializing the map
+    var map = new google.maps.Map(document.getElementById('map'),
+        {
+            zoom: 16,
+            center: OhioState,
+            restriction: { latLngBounds: { north: 40.006563, south: 39.995, west: -83.02374, east: -83.008 } }
+        });
     // Marker positioned at Ohio State
     var marker = new google.maps.Marker({ position: OhioState, map: map });
 }
