5d344c1fc2f3f7b4cfa2a6be07ff46104231c486
Mitch Morganti <rmorgantistvm@gmail.com>
2019-11-02 13:36:43 -0400

    i forgot what i did but something new

--- site.js
+++ site.js
@@ -1,8 +1,9 @@
 // JavaScript source code
 // Initialize and add the map
+var OhioState = { lat: 39.999, lng: -83.013 };
 function initMap() {
     // The location of Ohio State
-    var OhioState = { lat: 39.999, lng: -83.013 };
+
 
     // The map, centered at Ohio State
     //Initializing the map
@@ -12,6 +13,21 @@ function initMap() {
             center: OhioState,
             restriction: { latLngBounds: { north: 40.006563, south: 39.995, west: -83.02374, east: -83.008 } }
         });
+    //Stlying the map and hiding businesses
+    var mapStyle = [
+        {
+            featureType: "poi.business",
+            stylers: [{ visibility: "off", }]
+        },
+        {
+        featureType: 'road',
+        elementType: 'geometry',
+            stylers: [{ color: 'red' }]
+        }
+          ];
+        map.setOptions({ styles: mapStyle });
     // Marker positioned at Ohio State
-    var marker = new google.maps.Marker({ position: OhioState, map: map });
+    
+    var OhioUnionLoc = { lat: 39.998003, lng: -83.008852 };
+        var OhioUnion = new google.maps.Marker({ position: OhioUnionLoc, map: map })
 }
