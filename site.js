99a20e183596cc007a0cc0221cb918035e51a352
nathannewcomer <Alex1221999>
2019-11-02 16:24:36 -0400

    Added buttons in infoWindow

--- site.js
+++ site.js
@@ -1,9 +1,12 @@
 // JavaScript source code
 // Initialize and add the map
 var OhioState = { lat: 39.999, lng: -83.013 };
-function xyz(infowindow) {
-    infowindow.setContent("Blah")
-}
+
+var button = document.createElement("BUTTON");
+button.innerHTML = "YOYOYOYOYO";
+
+var infowindow;
+
 function initMap() {
     // The location of Ohio State
 
@@ -32,29 +35,19 @@ function initMap() {
     // Marker positioned at Ohio State
     
     var OhioUnionLoc = { lat: 39.998003, lng: -83.008852 };
-    var OhioUnion = new google.maps.Marker({ position: OhioUnionLoc, map: map, title: 'Ohio Union' })
-    var firstFloor = "<a href= '#' id='firstfloor'>First Floor</a>";
-    var contentString = '<div id="content">' +
-        '<div id="siteNotice">' +
-        '</div>' +
-        '<h1 id="firstHeading" class="firstHeading">Ohio Union</h1>' +
-        '<div id="bodyContent">' +
-        '<p><ul><li>Bathrooms: ' + firstFloor + ', Second Floor, Third Floor</li> <li>View Reviews</li></ul ></p > '
-    '</div>';
 
+    var OhioUnion = new google.maps.Marker({ position: OhioUnionLoc, map: map, title: 'Ohio Union' });
 
-    var infowindow = new google.maps.InfoWindow({
-        content: "<button onclick='xyz()'>First Floor</button>"
+    infowindow = new google.maps.InfoWindow({
+        content: button
     });
 
-    
-
-    
-    OhioUnion.addListener('click', function() {
+    OhioUnion.addListener('click', function () {
         infowindow.open(map, OhioUnion);
-    }
-    
-   )
+    });
    
-
 }
+
+button.addEventListener("click", function () {
+    infowindow.setContent("QWERTG");
+});
