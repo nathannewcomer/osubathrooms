63a293c14c2e158d1005e581c9a7fcb5da488918
nathannewcomer <Alex1221999>
2019-11-02 11:47:23 -0400

    Moved JavaScript to site.js

--- site.js
+++ site.js
@@ -1,1 +1,11 @@
 // JavaScript source code
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    var OhioState = { lat: 39.999, lng: -83.013 };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 16, center: OhioState });
+    // Marker positioned at Ohio State
+    var marker = new google.maps.Marker({ position: OhioState, map: map });
+}
